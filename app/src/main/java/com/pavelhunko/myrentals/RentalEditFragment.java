cd5119f5fbcc05d3088e3a023adecd007d2e83bb
Pavel Hunko <pavelhunko@gmail.com>
2015-09-02 16:47:06 +0300

    Initial commit

---
+++ app/src/main/java/com/pavelhunko/myrentals/RentalEditFragment.java
@@ -0,0 +1,57 @@
+package com.pavelhunko.myrentals;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class RentalEditFragment extends Fragment {
+
+    public static RentalEditFragment newInstance() {
+        RentalEditFragment fragment = new RentalEditFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RentalEditFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rental_edit, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+
+
+}
